dfad152e850f0880144e75519cbdfce6c7b505f7
qiaochao <qiaochao@icourt.cc>
2025-12-02 15:36:14 +0800

    feat: climb stairs

--- algorithm/5_Climbing_Stairs.go
+++ algorithm/5_Climbing_Stairs.go
@@ -1,29 +1,21 @@
 package algorithm
 
-//爬楼梯问题，斐波那契数列的应用
 func ClimbStairs(n int) int {
-	// 处理边界情况
-	if n <= 0 {
-		return 0
-	}
-	if n == 1 {
-		return 1
-	}
-	if n == 2 {
-		return 2
-	}
+    if n <= 0 { return 0 }
+    if n == 1 { return 1 }
+    if n == 2 { return 2 }
 
-	// 使用动态规划，dp[i] = dp[i-1] + dp[i-2]
-	// dp[i] 表示爬到第i阶台阶的方法数
-	prev1, prev2 := 1, 2 // 分别对应第1阶和第2阶的方法数
+    // dp[i] 表示爬到第i阶的方法数
+    dp := make([]int, n+1)
+    dp[0] = 0  // 0阶，0种方法
+    dp[1] = 1  // 1阶，1种方法
+    dp[2] = 2  // 2阶，2种方法
 
-	// 从第3阶开始计算
-	for i := 3; i <= n; i++ {
-		// 当前台阶的方法数 = 前两阶方法数之和
-		current := prev1 + prev2
-		prev1, prev2 = prev2, current
-	}
+    // 从第3阶开始递推
+    for i := 3; i <= n; i++ {
+		//到dp[i]的时候和前边dp组合的关系
+        dp[i] = dp[i-1] + dp[i-2]  // 斐波那契递推
+    }
 
-	// 最终 prev2 包含了爬到第n阶的方法数
-	return prev2
+    return dp[n]
 }
